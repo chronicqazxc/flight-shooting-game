5e577b19f28a914943392774daa2801dd82a2643
Wayne Hsiao <chronicqazxc@gmail.com>
2025-12-28 12:09:05 +0900

    Addressed project warning issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,9 +70,7 @@ android {
             jvmTarget.set(JvmTarget.JVM_17)
         }
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.6.0"
-    }
+
     buildFeatures {
         compose = true
         viewBinding = true
@@ -81,20 +79,20 @@ android {
 }
 
     dependencies {
-        implementation("androidx.core:core-ktx:1.13.1")
-        implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-        implementation("androidx.activity:activity-compose:1.9.1")
-        implementation("androidx.compose.ui:ui:1.6.0")
-        implementation("androidx.compose.material3:material3:1.2.1")
-        implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0")
-        implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.0")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
-        testImplementation("junit:junit:4.13.2")
-        testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
-        testImplementation("app.cash.turbine:turbine:1.1.0")
-        androidTestImplementation("androidx.test.ext:junit:1.2.1")
-        androidTestImplementation("androidx.test.espresso:espresso-core:3.6.0")
-        androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.0")
-        debugImplementation("androidx.compose.ui:ui-tooling:1.6.0")
+        implementation(libs.androidx.core.ktx)
+        implementation(libs.androidx.lifecycle.runtime.ktx)
+        implementation(libs.androidx.lifecycle.viewmodel.ktx)
+        implementation(libs.androidx.lifecycle.viewmodel.compose)
+        implementation(libs.androidx.activity.compose)
+        implementation(libs.androidx.compose.ui)
+        implementation(libs.androidx.compose.material3)
+        implementation(libs.kotlinx.coroutines.core)
+        implementation(libs.kotlinx.coroutines.android)
+        testImplementation(libs.junit)
+        testImplementation(libs.kotlinx.coroutines.test)
+        testImplementation(libs.turbine)
+        androidTestImplementation(libs.androidx.test.junit)
+        androidTestImplementation(libs.androidx.test.espresso.core)
+        androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+        debugImplementation(libs.androidx.compose.ui.tooling)
     }
